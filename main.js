e4addda01fde4a3dab67057c20c4b7fd55153585
vaibhav panvalkar <vpanvalkar@outlook.com>
2021-07-08 22:49:52 +0530

    server

--- main.js
+++ main.js
@@ -29,11 +29,12 @@ app.on('window-all-closed', function () {
 /**
  * Auto updater
  */
-https://github.com/panvalkar1994/Auto-updater-e/releases/download/v1.0.1/auto-updater-Setup-1.0.1.exe
-const server = 'https://github.com/panvalkar1994/Auto-updater-e'
-const url = `${server}/releases/download/${app.getVersion()}`
-// const url = `https://github.com/panvalkar1994/Auto-updater-e/releases/download/${app.getVersion()}`
+// https://github.com/panvalkar1994/Auto-updater-e/releases/download/v1.0.1/auto-updater-Setup-1.0.1.exe
+const server = 'https://github.com/panvalkar1994/Auto-updater-e';
+const url = `${server}/releases/download/${process.platform}/${app.getVersion()}`;
+
 autoUpdater.setFeedURL({ url })
+
  
 setInterval(() => {
   autoUpdater.checkForUpdates();
